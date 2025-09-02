d6af8f383a6d027b7c9a8ed26aca1227c24ebae4
Jyoti Mundargi <163743624+JyotiMundargi@users.noreply.github.com>
2025-09-02 18:46:42 +0530

    Add files via upload

---
+++ Rectangle.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Rectangle
+{
+    public:
+    int length;
+      int breadth;
+
+    Rectangle()
+    {
+        length=10;
+        breadth=20;
+        cout<<"the length of wall is: "<<length<<endl;
+         cout<<"the braedth of wall is: "<<breadth<<endl;
+    }
+
+    int calculateArea(){
+     return length*breadth;
+    }
+};
+
+int main()
+{
+   Rectangle rec;
+   cout<<"the area of rectangle is: "<<rec.calculateArea();
+    return 0;
+}
